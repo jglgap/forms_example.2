a182a9c73ac39b17c64d99de0043c8abdbb25722
jglgap <a23ricardosr@iessanclemente.net>
2025-01-30 14:32:11 +0100

    30/1

--- review/forms.py
+++ review/forms.py
@@ -1,9 +1,26 @@
 from django import forms
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your name",max_length=100,error_messages={
+#         "required": "your name musnt`t be empty",
+#         "max_length":"Please enter a shorter name"
+#     })
+#     review_text = forms.CharField(label="Your feedback",widget=forms.Textarea,max_length=200)
+#     rating = forms.IntegerField(label="your rating",min_value=1,max_value=5)
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your name",max_length=100,error_messages={
-        "required": "your name musnt`t be empty",
-        "max_length":"Please enter a shorter name"
-    })
-    review_text = forms.CharField(label="Your feedback",widget=forms.Textarea,max_length=200)
-    rating = forms.IntegerField(label="your rating",min_value=1,max_value=5)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields ='__all__'
+        # fields = `[user-name,...]` 
+        labels = {
+            "user_name" : "your name ",
+            "review_text" : "your review",
+            "rating" : "your rating"
+        }
+        error_messages = {
+            "user_name":{
+                "required" : "your name is required",
+                "max_lenght" : "your name must be shorter"
+            }
+        }
--- review/views.py
+++ review/views.py
@@ -2,18 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
+from django.views import View
+
 # Create your views here.
+
+class ReviewView(View):
+    def get(self,request):
+        form = ReviewForm()
+        return render (request,"review/review.html",{"form":form})
+    def post(self,request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            # view =Review(
+            #     user_name = form.cleaned_data['user_name'],
+            #     review_text = form.cleaned_data['review_text'],
+            #     rating = form.cleaned_data['rating']
+            # )
+            # view.save()
+            form.save()
+            return HttpResponseRedirect("/thank_u")
+
+
+
+
 def review(request):
     if request.method=='POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            view =Review(
-                user_name = form.cleaned_data['user_name'],
-                review_text = form.cleaned_data['review_text'],
-                rating = form.cleaned_data['rating']
-            )
-            view.save()
+            # print(form.cleaned_data)
+            # view =Review(
+            #     user_name = form.cleaned_data['user_name'],
+            #     review_text = form.cleaned_data['review_text'],
+            #     rating = form.cleaned_data['rating']
+            # )
+            # view.save()
+            form.save()
             return HttpResponseRedirect("/thank_u")
     else:
         form = ReviewForm()
